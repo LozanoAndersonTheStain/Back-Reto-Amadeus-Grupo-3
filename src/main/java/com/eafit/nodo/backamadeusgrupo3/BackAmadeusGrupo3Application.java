b27d4b53beeb638323208d91d4814ca1b855b310
Anderson Lozano <adslozanov@gmail.com>
2024-10-08 10:02:24 -0500

    Inicialited Project

---
+++ src/main/java/com/eafit/nodo/backamadeusgrupo3/BackAmadeusGrupo3Application.java
@@ -0,0 +1,14 @@
+package com.eafit.nodo.backamadeusgrupo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackAmadeusGrupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackAmadeusGrupo3Application.class, args);
+
+		System.out.println("Hola Mundo");
+	}
+}
---
+++ src/test/java/com/eafit/nodo/backamadeusgrupo3/BackAmadeusGrupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eafit.nodo.backamadeusgrupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackAmadeusGrupo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
